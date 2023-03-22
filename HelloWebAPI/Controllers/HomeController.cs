6cf293aa374e2bac03085ac49bd7250665ee2154
Halit Özken <halitozken@gmail.com>
2023-03-22 15:26:57 +0300

    Add project files.

---
+++ HelloWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using HelloWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWebAPI.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ResponseModel GetMessage()
+        {
+            return new ResponseModel()
+            {
+                HttpStatus = 200,
+                Message = "Hello ASP.NET Core Web API"
+            };
+        }
+
+    }
+}
